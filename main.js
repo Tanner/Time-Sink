157e300192f23258e64239d1356512c4a6f7f478
Tanner Smith <tanner@tssoftware.net>
2013-01-20 13:14:27 -0500

    Get initial brush context working.

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 var margin = {top: 10, right: 10, bottom: 100, left: 40};
+var margin2 = {top: 430, right: 10, bottom: 20, left: 40};
+
 var width = 960 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
+var height2 = 500 - margin2.top - margin2.bottom;
 
 var x = d3.time.scale().range([0, width]);
 var y = d3.scale.linear().range([0, height]);
 
+var x2 = d3.time.scale().range([0, width]);
+var y2 = d3.scale.linear().range([0, height2]);
+
 var xAxis = d3.svg.axis().scale(x).orient("bottom");
 var yAxis = d3.svg.axis().scale(y).orient("left");
+
+var xAxis2 = d3.svg.axis().scale(x2).orient("bottom");
+
+var brush = d3.svg.brush()
+  .x(x2)
+  .on("brush", brush);
 
 var svg = d3.select("body").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom);
 
+svg.append("defs").append("clipPath")
+  .attr("id", "clip")
+  .append("rect")
+  .attr("width", width)
+  .attr("height", height);
+
 var focus = svg.append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var context = svg.append("g")
+  .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
 d3.csv("yesterday.csv", function(error, csv) {
   var data = d3.nest()
@@ -43,6 +64,8 @@ d3.csv("yesterday.csv", function(error, csv) {
     })
   ]);
 
+  x2.domain(x.domain());
+
   y.domain([0, data.length]);
 
   focus.append("g")
@@ -66,5 +89,39 @@ d3.csv("yesterday.csv", function(error, csv) {
     .append("rect")
     .attr("x", function(d) { return x(d.foreground_begin); })
     .attr("height", rect_height)
-    .attr("width", function(d) { return x(d.foreground_end) - x(d.foreground_begin); });
+    .attr("width", function(d) { return x(d.foreground_end) - x(d.foreground_begin); })
+    .attr("clip-path", "url(#clip)");
+
+  var apps2 = context.selectAll(".app")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "app")
+    .attr("transform", function(d, i) { return "translate(0, " + (height - y2(i)) + ")"; });
+
+  apps2.selectAll("rect")
+    .data(function(d) { return d.values; })
+    .enter()
+    .append("rect")
+    .attr("x", function(d) { return x2(d.foreground_begin); })
+    .attr("height", rect_height)
+    .attr("width", function(d) { return x2(d.foreground_end) - x(d.foreground_begin); });
+
+  context.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, " + height2 + ")")
+    .call(xAxis2);
+
+  context.append("g")
+    .attr("class", "x brush")
+    .call(brush)
+    .selectAll("rect")
+    .attr("y", -6)
+    .attr("height", height2 + 7);
 });
+
+function brush() {
+  x.domain(brush.empty() ? x2.domain() : brush.extent());
+
+  focus.select(".x.axis").call(xAxis);
+}
