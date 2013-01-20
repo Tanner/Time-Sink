ab0595b49f8a4a81eb53d5ce2f6a11889e08644d
Tanner Smith <tanner@tssoftware.net>
2013-01-19 21:55:07 -0500

    Load data, fix dates and create x axis.

---
+++ main.js
@@ -0,0 +1,50 @@
+var margin = {top: 10, right: 10, bottom: 100, left: 40};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([0, height]);
+
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
+
+var focus = svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("yesterday.csv", function(error, csv) {
+  var data = d3.nest()
+    .key(function(d) { return d["Application"]; })
+    .sortKeys(d3.ascending)
+    .entries(csv);
+
+  console.log(data);
+
+  data.forEach(function(app) {
+    app.values.forEach(function(values) {
+      values.foreground_begin = new Date(values["Foreground Begin (Unix Time)"] * 1000);
+      values.foreground_end = new Date(values["Foreground End (Unix Time)"] * 1000);
+    });
+  });
+
+  x.domain([
+    d3.min(data, function(d) {
+      return d3.min(d.values, function(v) {
+        return v.foreground_begin;
+      });
+    }),
+    d3.max(data, function(d) {
+      return d3.min(d.values, function(v) {
+        return v.foreground_end;
+      });
+    })
+  ]);
+
+  focus.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+});
